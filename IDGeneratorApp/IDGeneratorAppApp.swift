bae8321de67789e8a82dc0033d9e67c4bd0dd6df
Aviie <5209403+ilife-aviie@users.noreply.github.com>
2020-07-16 12:58:52 +0530

    Initial Commit

---
+++ IDGeneratorApp/IDGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDGeneratorAppApp.swift
+//  IDGeneratorApp
+//
+//  Created by DJ on 7/16/20.
+//
+
+import SwiftUI
+
+@main
+struct IDGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
